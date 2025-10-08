67af65b6a035a137ab490824eec6874cd5797fab
unknown <ueki@taksam.co.jp>
2025-10-08 09:36:57 +0900

    fix: Correct JSX syntax in AnalyticsPageContent

--- src/app/evaluations/analytics/AnalyticsPageContent.tsx
+++ src/app/evaluations/analytics/AnalyticsPageContent.tsx
@@ -212,7 +212,6 @@ export default function AnalyticsPageContent() {
                             </Row>
                         </>
                     )
-                }
             </main>
         </div>
     );
